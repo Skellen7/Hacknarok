16c6826d8fa398216e657116f7e6d4c3fdf709e2
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2018-03-11 00:40:05 +0100

    Setting names & switching activities

--- app/src/main/java/com/example/kuba/sloik/MainActivity.java
+++ app/src/main/java/com/example/kuba/sloik/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kuba.sloik;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -104,17 +105,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_info) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+        } else if (id == R.id.nav_inventory) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_list) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_history) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
 
         } else if (id == R.id.nav_share) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
 
         } else if (id == R.id.nav_send) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
 
         }
 
